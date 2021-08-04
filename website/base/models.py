2eccaada95d9ba3401444236373e80f49575e94a
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2021-08-04 22:45:23 +0200

    fix some database stuff

--- website/base/models.py
+++ website/base/models.py
@@ -1,19 +1,28 @@
 from django.db import models
 from django.utils import timezone
+from bs4 import BeautifulSoup
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    _snippet = models.TextField(blank=True)
+    _date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
+
+    @property
+    def date_posted(self):
+        return self._date_posted.strftime(r'%Y-%m-%dT%H:%M:%SZ')
     
+    @property
+    def snippet(self):
+        return self._snippet or BeautifulSoup(self.content, 'html.parser').p.string
+
     @property
     def relative_url(self):
         return f'../../post/{self.pk}'
-
 
     
 
